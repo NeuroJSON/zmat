76797af3076de1efd792b4c3b40850b9aaa0610f
Qianqian Fang <fangq@Luban-5.local>
2019-05-02 13:39:58 -0400

    compile on matlab 2018 and octave 5.0 or later

--- src/zmat.cpp
+++ src/zmat.cpp
@@ -26,12 +26,7 @@
 #include "mex.h"
 #include "zlib.h"
 
-#if (! defined MX_API_VER) || (MX_API_VER < 0x07300000)
-      typedef int dimtype;                              /**<  MATLAB before 2017 uses int as the dimension array */
-#else
-      typedef size_t dimtype;                           /**<  MATLAB after 2017 uses size_t as the dimension array */
-#endif
-
+typedef mwSize dimtype;
 
 void zmat_usage();
 int  zmat_keylookup(char *origkey, const char *table[]);
