d8cd44049a588febaccdfc83d00da3d211d92897
Qianqian Fang <fangqq@gmail.com>
2019-05-04 18:54:55 -0400

    apply patch to compile on newer matlab and octave

--- src/zmat.cpp
+++ src/zmat.cpp
@@ -26,12 +26,7 @@
 #include "mex.h"
 #include "zlib.h"
 
-#if (! defined MX_API_VER) || (MX_API_VER < 0x07300000)
-      typedef int dimtype;                              /**<  MATLAB before 2017 uses int as the dimension array */
-#else
-      typedef size_t dimtype;                           /**<  MATLAB after 2017 uses size_t as the dimension array */
-#endif
-
+typedef mwSize dimtype;
 
 void zmat_usage();
 int  zmat_keylookup(char *origkey, const char *table[]);
@@ -139,7 +134,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]){
 
                 	while((ret=inflate(&zs, Z_SYNC_FLUSH))!=Z_STREAM_END && count<=10){
 			    temp=(unsigned char *)realloc(temp, (buflen[0]<<count));
-			    zs.next_out =  (Bytef *)(temp+(buflen[0]<<(count-1))); //(Bytef *)(); // output char array
+			    zs.next_out =  (Bytef *)(temp+(buflen[0]<<(count-1)));
 			    zs.avail_out = (buflen[0]<<(count-1)); // size of output
 			    count++;
 			}
