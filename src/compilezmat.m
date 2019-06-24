274ce377e2f4869e19c1e68d4f4940cb32a87bc5
Qianqian Fang <fangqq@gmail.com>
2019-06-24 18:51:48 -0400

    compile zmat on octave 5

--- src/compilezmat.m
+++ src/compilezmat.m
@@ -3,6 +3,6 @@ if(~exist('OCTAVE_VERSION','builtin'))
     mex zmat.cpp zmatlib.o easylzma/easylzma-0.0.8/lib/libeasylzma_s.a -Ieasylzma/easylzma-0.0.8/include -outdir ../ CXXLIBS='$CXXLIBS -lz'
 else
     mex -Ieasylzma/easylzma-0.0.8/include -c zmatlib.c
-    mex zmat.cpp zmatlib.o easylzma/easylzma-0.0.8/lib/libeasylzma_s.a -Ieasylzma/easylzma-0.0.8/include -o ../zmat
+    mex zmat.cpp zmatlib.o easylzma/easylzma-0.0.8/lib/libeasylzma_s.a -Ieasylzma/easylzma-0.0.8/include -o ../zmat -lz
 end
 
