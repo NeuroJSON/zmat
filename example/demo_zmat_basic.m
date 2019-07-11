75ec306e1c8e2fe54e8d58114870cd50e87be22b
Qianqian Fang <fangqq@gmail.com>
2019-07-11 13:49:34 -0400

    update changelog, update demo script

--- example/demo_zmat_basic.m
+++ example/demo_zmat_basic.m
@@ -13,3 +13,13 @@ char(base64)
 orig=zmat(base64,0,'base64');
 char(orig)
 
+% encode ND numeric array
+orig=single(rand(5));
+[Aencoded,info]=zmat(orig,1,'lzma')
+
+% decode compressed ND array and restore size/type
+Adecoded=zmat(Aencoded,info)
+
+all(all(Adecoded==orig))
+class(Adecoded)
+
