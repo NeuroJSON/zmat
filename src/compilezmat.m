6dfd214556456e3e10fe81d71fd4ec9543a71406
Qianqian Fang <fangqq@gmail.com>
2020-05-23 18:21:31 -0400

    fix compilezmat for linux

--- src/compilezmat.m
+++ src/compilezmat.m
@@ -10,17 +10,15 @@ if(ispc)
     suffix='.obj';
 end
 if(~exist('OCTAVE_VERSION','builtin'))
-    CCFLAG='CFLAGS=''-O3 -g -Ieasylzma -Ieasylzma/pavlov -Ilz4'' -c';
-    LINKFLAG='CXXLIBS=''-lz'' -output ../zipmat -outdir ../';
+    CCFLAG='CFLAGS=''-O3 -g -Ieasylzma -Ieasylzma/pavlov -Ilz4 -fPIC'' -c';
+    LINKFLAG='CXXLIBS=''\$CLIBS -lz'' -output ../zipmat -outdir ../';
     for i=1:length(filelist)
         fprintf(1,'mex %s %s\n', CCFLAG, filelist{i});
         eval(sprintf('mex %s %s', CCFLAG, filelist{i}));
     end
-    if(ispc)
-        filelist=dir('*.obj');
-        filelist={filelist.name};
-    end
-    cmd=sprintf('mex %s -Ieasylzma %s %s',mexfile, LINKFLAG, regexprep(sprintf('%s ' ,filelist{:}),'\.c[p]*',suffix));
+    filelist=dir(['*' suffix]);
+    filelist={filelist.name};
+    cmd=sprintf('mex %s -Ieasylzma %s %s',mexfile, LINKFLAG, sprintf('%s ' ,filelist{:}));
     fprintf(1,'%s\n',cmd);
     eval(cmd)
 else
