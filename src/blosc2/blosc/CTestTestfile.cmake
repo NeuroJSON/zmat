65ae09f890349cd171e12820a1728e30527a3d77
Qianqian Fang <fangqq@gmail.com>
2022-09-10 11:51:40 -0400

    add blosc2 code tree

---
+++ src/blosc2/blosc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fangq/space/git/Temp/c-blosc2/blosc
+# Build directory: /home/fangq/space/git/Temp/c-blosc2/blosc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
