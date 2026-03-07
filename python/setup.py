1539419dbf4d0c34d8eded266c5a2613c8307c39
Qianqian Fang <fangqq@gmail.com>
2026-03-07 12:39:42 -0500

    [ci] fix macos-15-intel error

--- python/setup.py
+++ python/setup.py
@@ -102,8 +102,10 @@ if use_zstd:
         pattern = os.path.join(zstd_dir, subdir, "*.c")
         sources.extend(glob.glob(pattern))
 
-    # zstd assembly is x86_64 only; disable on ARM and other platforms
-    if not is_x86:
+    # zstd assembly requires GNU as and GNU ld; disable on non-Linux
+    # platforms (macOS linker doesn't support zstd's x86_64 asm, and
+    # ARM/other architectures don't have it at all)
+    if platform.system() != "Linux" or not is_x86:
         define_macros.append(("ZSTD_DISABLE_ASM", "1"))
 else:
     define_macros.append(("NO_ZSTD", None))
